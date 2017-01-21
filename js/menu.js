a9564a9b54e44f18f790d5e71f01ce8f7f4a3f91
Mledev <le.devehat.matth@gmail.com>
2017-01-21 16:34:12 +0100

    Menu fini sans responsive

---
+++ js/menu.js
@@ -0,0 +1,8 @@
+$(document).ready( function() {
+    $('.barres').click(function(e){
+        $('nav').toggleClass('hidden');
+        $('.menucache ul').hide(0).delay(300).show(200);
+        $('.menucache li').hide(0).delay(500).show(200);
+        e.stopPropagation();
+    })
+});
