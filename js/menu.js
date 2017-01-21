ecf132390c70091bd376c44955475b177f76eeea
Mledev <le.devehat.matth@gmail.com>
2017-01-21 17:52:44 +0100

    menu non responsive
    Fini, en attente de debug

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 $(document).ready( function() {
     $('.barres').click(function(e){
         $('nav').toggleClass('hidden');
+        $('body').toggleClass('hidden');
         $('.menucache ul').hide(0).delay(300).show(200);
         $('.menucache li').hide(0).delay(500).show(200);
         e.stopPropagation();
