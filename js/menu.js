652ac9a8ff34d3205d5e3c23c23cf26a8af50f96
Mledev <le.devehat.matth@gmail.com>
2017-01-22 18:01:55 +0100

    Menu responsive
    A debug

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,19 @@
-$(document).ready( function() {
+$(document).ready(function () {
 
     $('body').addClass('hidden');
+    $('.champs input').addClass('cache');
 
-    $('.barres').click(function(e){
+    $('.barres').click(function (e) {
         $('nav').toggleClass('hidden');
         $('body').toggleClass('hidden');
         $('.menucache ul').hide(0).delay(300).show(200);
         $('.menucache li').hide(0).delay(500).show(200);
         e.stopPropagation();
+    })
+
+    $('.champs .fa-search').click(function () {
+        if ($('.champs input').hasClass('cache') == true) {
+            $('.champs input').removeClass('cache');
+        }
     })
 });
