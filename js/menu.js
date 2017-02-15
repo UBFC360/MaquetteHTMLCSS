e72ece6caa3a39e9efc23eda36a0a73c58cbc09e
ValDespierres <despierres.valentin@gmail.com>
2017-02-15 02:26:18 +0100

    menu qui marche bien

--- js/menu.js
+++ js/menu.js
@@ -6,10 +6,12 @@ $(document).ready(function () {
     $('.barres').click(function (e) {
         $('nav').toggleClass('hidden');
         $('body').toggleClass('hidden');
+        $('body.acc').css('overflow-y','hidden');
+        $('body.acc.hidden').css('overflow-y','auto');
         $('.menucache ul').hide(0).delay(300).show(200);
         $('.menucache li').hide(0).delay(500).show(200);
         e.stopPropagation();
-    })
+    });
 
     $('.champs .fa-search').click(function () {
         if ($('.champs input').hasClass('cache') == true) {
